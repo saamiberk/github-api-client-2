ced9181e3d0ebde3ba54b04de838e3e902c1df0b
sami <saamiberk@gmail.com>
2019-11-28 10:12:00 +0300

    commit1

---
+++ src/main/java/com/hitit/githubclient/model/response/Author.java
@@ -0,0 +1,22 @@
+package com.hitit.githubclient.model.response;
+
+public class Author {
+    private String login;
+    private int id;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
